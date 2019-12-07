88e64591fb3a99ba318615382bc993ed24474b80
nikel <r0075301net.r@yandex.ru>
2019-12-07 16:21:13 +0300

    refactoring
    Ага, он самый

--- puzzleCLI.py
+++ puzzleCLI.py
@@ -6,79 +6,77 @@ def input(prompt):
     return oldinput('>>> ')
 
 #######################################################################
+# Установка иконки на папку
 import os
-import ctypes
-from ctypes import POINTER, Structure, c_wchar, c_int, sizeof, byref
-from ctypes.wintypes import BYTE, WORD, DWORD, LPWSTR, LPSTR
-HICON = c_int
-LPTSTR = LPWSTR
-TCHAR = c_wchar
-MAX_PATH = 260
-FCSM_ICONFILE = 0x00000010
-FCS_FORCEWRITE = 0x00000002
-SHGFI_ICONLOCATION = 0x000001000
+isWindows = os.name
+if isWindows:
+    import ctypes
+    from ctypes import POINTER, Structure, c_wchar, c_int, sizeof, byref
+    from ctypes.wintypes import BYTE, WORD, DWORD, LPWSTR, LPSTR
+    HICON = c_int
+    LPTSTR = LPWSTR
+    TCHAR = c_wchar
+    MAX_PATH = 260
+    FCSM_ICONFILE = 0x00000010
+    FCS_FORCEWRITE = 0x00000002
+    SHGFI_ICONLOCATION = 0x000001000
 
-class GUID(Structure):
-    _fields_ = [
-        ('Data1', DWORD),
-        ('Data2', WORD),
-        ('Data3', WORD),
-        ('Data4', BYTE * 8)]
+    class GUID(Structure):
+        _fields_ = [
+            ('Data1', DWORD),
+            ('Data2', WORD),
+            ('Data3', WORD),
+            ('Data4', BYTE * 8)]
 
-class SHFOLDERCUSTOMSETTINGS(Structure):
-    _fields_ = [
-        ('dwSize', DWORD),
-        ('dwMask', DWORD),
-        ('pvid', POINTER(GUID)),
-        ('pszWebViewTemplate', LPTSTR),
-        ('cchWebViewTemplate', DWORD),
-        ('pszWebViewTemplateVersion', LPTSTR),
-        ('pszInfoTip', LPTSTR),
-        ('cchInfoTip', DWORD),
-        ('pclsid', POINTER(GUID)),
-        ('dwFlags', DWORD),
-        ('pszIconFile', LPTSTR),
-        ('cchIconFile', DWORD),
-        ('iIconIndex', c_int),
-        ('pszLogo', LPTSTR),
-        ('cchLogo', DWORD)]
+    class SHFOLDERCUSTOMSETTINGS(Structure):
+        _fields_ = [
+            ('dwSize', DWORD),
+            ('dwMask', DWORD),
+            ('pvid', POINTER(GUID)),
+            ('pszWebViewTemplate', LPTSTR),
+            ('cchWebViewTemplate', DWORD),
+            ('pszWebViewTemplateVersion', LPTSTR),
+            ('pszInfoTip', LPTSTR),
+            ('cchInfoTip', DWORD),
+            ('pclsid', POINTER(GUID)),
+            ('dwFlags', DWORD),
+            ('pszIconFile', LPTSTR),
+            ('cchIconFile', DWORD),
+            ('iIconIndex', c_int),
+            ('pszLogo', LPTSTR),
+            ('cchLogo', DWORD)]
 
-class SHFILEINFO(Structure):
-    _fields_ = [
-        ('hIcon', HICON),
-        ('iIcon', c_int),
-        ('dwAttributes', DWORD),
-        ('szDisplayName', TCHAR * MAX_PATH),
-        ('szTypeName', TCHAR * 80)]
+    class SHFILEINFO(Structure):
+        _fields_ = [
+            ('hIcon', HICON),
+            ('iIcon', c_int),
+            ('dwAttributes', DWORD),
+            ('szDisplayName', TCHAR * MAX_PATH),
+            ('szTypeName', TCHAR * 80)]
 
-def seticon(folderpath, iconpath, iconindex):
-    """Set folder icon.
+    def seticon(folderpath, iconpath, iconindex):
+        """Set folder icon.
 
-    >>> seticon(".", "C:\\Windows\\system32\\SHELL32.dll", 10)
+        >>> seticon(".", "C:\\Windows\\system32\\SHELL32.dll", 10)
 
-    """
-    shell32 = ctypes.windll.shell32
+        """
+        shell32 = ctypes.windll.shell32
 
-    folderpath = os.path.abspath(folderpath)
-    iconpath = os.path.abspath(iconpath)
+        folderpath = os.path.abspath(folderpath)
+        iconpath = os.path.abspath(iconpath)
 
-    fcs = SHFOLDERCUSTOMSETTINGS()
-    fcs.dwSize = sizeof(fcs)
-    fcs.dwMask = FCSM_ICONFILE
-    fcs.pszIconFile = iconpath
-    fcs.cchIconFile = 0
-    fcs.iIconIndex = iconindex
+        fcs = SHFOLDERCUSTOMSETTINGS()
+        fcs.dwSize = sizeof(fcs)
+        fcs.dwMask = FCSM_ICONFILE
+        fcs.pszIconFile = iconpath
+        fcs.cchIconFile = 0
+        fcs.iIconIndex = iconindex
 
-    hr = shell32.SHGetSetFolderCustomSettings(byref(fcs), folderpath, FCS_FORCEWRITE)
-    if hr:
-        raise WindowsError('Ахуеть!')
+        sfi = SHFILEINFO()
 
-    sfi = SHFILEINFO()
-    hr = shell32.SHGetFileInfoW(folderpath, 0, byref(sfi), sizeof(sfi), SHGFI_ICONLOCATION)
+        index = shell32.Shell_GetCachedImageIndexW(sfi.szDisplayName, sfi.iIcon, 0)
 
-    index = shell32.Shell_GetCachedImageIndexW(sfi.szDisplayName, sfi.iIcon, 0)
-
-    shell32.SHUpdateImageW(sfi.szDisplayName, sfi.iIcon, 0, index)
+        shell32.SHUpdateImageW(sfi.szDisplayName, sfi.iIcon, 0, index)
 
 #######################################################################
 
@@ -183,24 +181,23 @@ isExists = False
 while not isExists:
     imgName = input('Введите имя файла картинки, которая лежит в одной папке с программой:')
     if imgName and os.path.isfile(imgName):
-        isExists = True
+        print('Файл найден.')
     elif os.path.isfile('input.jpg'):
-        print('По заданному пути файл не найден, кроме заданного по умолчанию: input.jpg\n')
-        isExists = True
+        print('По заданному пути файл не найден, кроме заданного по умолчанию: test.jpg\n')
         imgName = 'input.jpg'
     else:
         print('Файл с картинкой по указанному пути не найден! Попробуйте снова...\n')
+        continue
 
-    if isExists:
-        try:
-            img = Image.open(imgName).convert('RGBA')
-        except:
-            print(f'Файл {imgName} скорее всего не является картинкой. Попробуйте другую картинку.\n')
-            isExists = False
-        else:
-            imgWidth = img.size[0]
-            imgHeight = img.size[1]
-            print(f'Картинка с разрешением {imgWidth} * {imgHeight} успешно открыта.\n')
+    try:
+        img = Image.open(imgName).convert('RGBA')
+    except:
+        print(f'Файл {imgName} скорее всего не является картинкой. Попробуйте другую картинку.\n')
+    else:
+        imgWidth = img.size[0]
+        imgHeight = img.size[1]
+        print(f'Картинка с разрешением {imgWidth} * {imgHeight} успешно открыта.\n')
+        isExists = True
 
 # Обнаружение рабочего стола пользователя
 try:
